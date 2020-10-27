ab04339eaa0cc2d398c2f5ec6c4e891d15864cc6
Federico <fwalovnik@gmail.com>
2020-10-27 18:02:49 -0300

    fixed images on github pages

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const data = [
     lanzamiento: '11/21/1976',
     textoPrincipal:
       'Sylvester Stallone fue quien le dio vida a Rocky Balboa, no solo actoralmente sino también fue quién escribió el guión de la propia película. Confesó que modificó tantas veces el guión original, que cree que solo conservó un diez por ciento en la trama final. ¿queres saber más? Entra aquí.',
-    imagenUrl: './imagenes/hero_rocky.png'
+    imagenUrl: '/imagenes/hero_rocky.png'
   },
   {
     titulo: 'Blade Runer 2049',
@@ -13,7 +13,7 @@ const data = [
     lanzamiento: '10/05/2017',
     textoPrincipal:
       'La página web oficial de la película, a pedido de los fans, ofrece una cronología de lo que sucedió entre los años 2019 y 2049, explican qué ocurrió con la Corporación Tyrell tras la muerte de su fundador, cuál es el destino de los replicantes y quién es Niander Wallace, el villano de la secuela.',
-    imagenUrl: './imagenes/hero_blade.png'
+    imagenUrl: '/imagenes/hero_blade.png'
   },
   {
     titulo: 'Bastardos Sin Gloria',
@@ -21,7 +21,7 @@ const data = [
     lanzamiento: '21/08/2009',
     textoPrincipal:
       '10 años de su vida fue el tiempo que le tomó al director Quentin Tarantino dar por finalizado la historia de la película, ya que no encontraba un final impresionante que acabara con su film. Comenzó cerca del año 1994, cuando estrenó Pulp Fiction, logró terminar de escribir antes que se lanzara Kill Bill.',
-    imagenUrl: './imagenes/hero_bastardos.png'
+    imagenUrl: '/imagenes/hero_bastardos.png'
   },
   {
     titulo: 'The Dark Knight',
@@ -29,7 +29,7 @@ const data = [
     lanzamiento: '18/07/2008',
     textoPrincipal:
       'Ya todos estamos al tanto de que la historia de origen de Batman..asi que aquí el increíble dato es que sorpresivamente en esta película no aparece ninguna versión de murciélagos por primera vez en todas las películas. Ahora la pregunta ¿fue a propósito o un desliz del director?',
-    imagenUrl: './imagenes/hero_batman.png'
+    imagenUrl: '/imagenes/hero_batman.png'
   },
   {
     titulo: 'Todas las novedades',
@@ -37,7 +37,7 @@ const data = [
     lanzamiento: '',
     textoPrincipal:
       'Conocé la nueva sección de Netflix donde te vamos a mostrar en primicia los detalles más increíbles de los estrenos del mes. El género de la película no influirá en la enorme cantidad de datos asombrosos que traeremos ¿Estás listo? ¡No te lo pierdas! Animate a ser parte de esta nueva sección.',
-    imagenUrl: './imagenes/hero_inactiva.png'
+    imagenUrl: '/imagenes/hero_inactiva.png'
   }
 ];
 
